ceb279880669197b6a9de99147411f9dc86cf44a
Usman Khokhar <usman.khokhar@systemsltd.com>
2022-06-03 18:56:17 +0500

    Apointment Controller updated

--- BeauitySaloonWeb/Controllers/AppointmentsController.cs
+++ BeauitySaloonWeb/Controllers/AppointmentsController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using BeauitySaloonWeb.Models;
+using BeauitySaloonWeb.Models.ViewModel.Appointments;
+using BeauitySaloonWeb.Models.ViewModel.SalonServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +11,90 @@ using System.Web.Mvc;
 namespace BeauitySaloonWeb.Controllers
 {
     public class AppointmentsController : Controller
-    {
+    {       
+
+        private static ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
+
         // GET: Appointments
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult MakeAnAppointment(string salonId, int serviceId)
+        {
+            try
+            {
+                Mapper.CreateMap<Salon, SalonServiceSimpleViewModel>();
+                var salonService = Mapper.Map<SalonServiceSimpleViewModel>(_applicationDbContext.SalonServices.Where(x=>x.SalonId==salonId &&
+                    x.ServiceId == serviceId).SingleOrDefault());
+
+                if (salonService == null || !salonService.Available)
+                {
+                    ViewBag.Exceptions = "No Appointment Service Found...!";
+                    return this.View("Error");
+                }
+
+                var viewModel = new AppointmentInputModel
+                {
+                    SalonId = salonId,
+                    ServiceId = serviceId,
+                };
+                return this.View(viewModel);
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Exceptions = ex.Message.ToString();
+                return this.View("Error");
+            }
+        } 
+
+        public ActionResult RatePastAppointment(string id)
+        {
+            try
+            {
+                var viewModel = new SalonServiceSimpleViewModel();
+                Mapper.CreateMap<Salon, AppointmentRatingViewModel>();
+                SalonService obj = _applicationDbContext.SalonServices.Where(x => x.SalonId == id).SingleOrDefault();
+                if (obj != null)
+                {
+                    viewModel = Mapper.Map<SalonServiceSimpleViewModel>(obj);
+                }
+                return this.View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Exceptions = ex.Message;
+                return this.View("Error");
+            }
+           
+        } 
+
+        [HttpGet]
+        public ActionResult CancelAppointment(string id)
+        {
+            try
+            {
+                var viewModel = new SalonServiceSimpleViewModel();
+                Mapper.CreateMap<Salon, AppointmentsController>();
+                SalonService obj = _applicationDbContext.SalonServices.Where(x => x.SalonId == id).SingleOrDefault();
+
+
+                if (obj != null)
+                {
+                    viewModel = Mapper.Map<SalonServiceSimpleViewModel>(obj);
+                }
+
+                return this.View(viewModel);
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Exceptions = ex.Message;
+                return this.View("Error");
+            }
+            
+        } 
+
     }
 }
---
+++ BeauitySaloonWeb/Controllers/BlogPostsController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using BeauitySaloonWeb.Models;
+using BeauitySaloonWeb.Models.ViewModel.BlogPosts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeauitySaloonWeb.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
+
+        // GET: BlogPosts
+        public ActionResult Index()
+        {
+
+            try
+            {
+                var viewModel = new BlogPostsListViewModel();
+
+                IEnumerable<BlogPost> list = _applicationDbContext.BlogPosts.ToList();
+
+                Mapper.CreateMap<BlogPost, BlogPostViewModel>();
+                if (list.Any())
+                {
+                    viewModel.BlogPosts = Mapper.Map<IEnumerable<BlogPostViewModel>>(list); //does not work, "cannot convert from 'System.Collections.Generic.IEnumerable<BloodDonatorsApp.Models.Donation>' to 'BloodDonatorsApp.Models.Donation'
+                }
+                return View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Exception = ex.Message.ToString();
+                return View("Error");
+            }
+            
+        }
+    }
+}
